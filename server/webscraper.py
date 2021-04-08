0f9f779a7ef4ab4762f71fd43f6dd676b349e9b0
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-04-08 12:26:11 -0700

    #2 test dbconnection and insert test document

--- server/webscraper.py
+++ server/webscraper.py
@@ -2,5 +2,19 @@ from dbconnection.connection import DbContext
 
 Context = DbContext
 MongoDbContext = Context().context
+database = MongoDbContext.PyNews
 
-print(MongoDbContext)
+
+testObj = {
+    "title": "Test Title",
+    "data": [
+        100,
+        200,
+        300
+    ]
+}
+
+articles = database.articles
+result = articles.insert_one(testObj)
+print(result)
+print(f"One test object: {result.inserted_id}")
