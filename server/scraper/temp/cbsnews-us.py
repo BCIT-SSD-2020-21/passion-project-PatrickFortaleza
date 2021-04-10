18bb858ab778943020f01dc1298c9c042937223e
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-04-10 14:24:38 -0700

    #9 webscrape script cbs news

--- server/scraper/temp/cbsnews-us.py
+++ server/scraper/temp/cbsnews-us.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import re
 from datetime import date
 from bs4 import BeautifulSoup
 
@@ -13,33 +14,33 @@ driver = webdriver.Firefox()
 
 today = date.today().isoformat()
 articles = []
-url = "https://www.foxnews.com/"
+url = "https://www.cbsnews.com/us/"
 
 driver.get(url)
 
 try:
     WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, "//main[@class='main-content']")))
+        (By.XPATH, "//section[@id='component-topic-us']")))
 finally:
     innerHTML = driver.execute_script(
         "window.scrollTo(0, document.body.scrollHeight-10000);var lenOfPage=document.body.scrollHeight;return document.body.innerHTML;")
 
     page_soup = BeautifulSoup(innerHTML, "html.parser")
-    mainSection = page_soup.find("main", {"class": "main-content"})
-    newsColumns = page_soup.find_all("div", {"class": "collection-spotlight"})
+    mainSection = page_soup.find("section", {"id": "component-topic-us"})
+    newsColumns = page_soup.find_all(
+        "div", {"class": "component__item-wrapper"})
 
     articlesList = []
     for column in newsColumns:
         articles = column.find_all("article")
-        for article in articles:
+        for article in articles[:5]:
             try:
-                h2 = article.find("h2", {"class": "title"})
-                headline = h2.find("a")
-                link = headline["href"]
-                text = headline.get_text()
+                link = article.find("a")["href"]
+                text = article.find("h4").get_text()
+                text = re.sub("\s\s", "", text)
 
                 article_ = {
-                    "site": "6070628dbcdb9a31a261bb97",  # Hardcoded ObjectId
+                    "site": "6072168fa55796cad11d99ea",  # Hardcoded ObjectId
                     "headline": text,
                     "article_url": link,
                     "date": today
