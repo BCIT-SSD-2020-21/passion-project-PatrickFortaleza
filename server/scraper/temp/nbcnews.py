3a99ae7d5b51a893639d6e124b00b2847fd0c240
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-04-11 10:59:13 -0700

    #9 implement scraping script for nbc

--- server/scraper/temp/nbcnews.py
+++ server/scraper/temp/nbcnews.py
@@ -14,34 +14,37 @@ driver = webdriver.Firefox()
 
 today = date.today().isoformat()
 articles = []
-url = "https://www.cbsnews.com/us/"
+url = "https://www.nbcnews.com/"
 
 driver.get(url)
 
 try:
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, "//section[@id='component-topic-us']")))
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located(
+        (By.XPATH, "//div[@class='layout-grid-item']")))
 finally:
     innerHTML = driver.execute_script(
         "window.scrollTo(0, document.body.scrollHeight-10000);var lenOfPage=document.body.scrollHeight;return document.body.innerHTML;")
 
     page_soup = BeautifulSoup(innerHTML, "html.parser")
-    mainSection = page_soup.find("section", {"id": "component-topic-us"})
+    mainSection = page_soup.find("section", {"class": "pkg-index-1"})
     newsColumns = page_soup.find_all(
-        "div", {"class": "component__item-wrapper"})
+        "div", {"class": "multi-up__article"})
 
     articlesList = []
-    for column in newsColumns[:2]:
+    for column in newsColumns:
         articles = column.find_all("article")
-        for article in articles[:5]:
+        for article in articles:
             try:
                 link = article.find("a")["href"]
-                text = article.find("h4").get_text()
-                text = re.sub("\s\s", "", text)
-                text = re.sub("\n", "", text)
+                info = article.find("div", {"class": "tease-card__info"})
+                text = info.find(
+                    "span", {"class": "tease-card__headline"}).getText()
+                # text = article.find("h3", {"class": "headline"}).get_text()
+                # text = re.sub("\t", "", text)
+                # text = re.sub("\n", "", text)
 
                 article_ = {
-                    "site": "6072168fa55796cad11d99ea",  # Hardcoded ObjectId
+                    "site": "60721d26a55796cad11d99ec",  # Hardcoded ObjectId
                     "headline": text,
                     "article_url": link,
                     "date": today
--- server/scraper/temp/nypost.py
+++ server/scraper/temp/nypost.py
@@ -14,34 +14,34 @@ driver = webdriver.Firefox()
 
 today = date.today().isoformat()
 articles = []
-url = "https://www.cbsnews.com/us/"
+url = "https://nypost.com/"
 
 driver.get(url)
 
 try:
     WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, "//section[@id='component-topic-us']")))
+        (By.XPATH, "//div[@id='home-page-wrapper']")))
 finally:
     innerHTML = driver.execute_script(
         "window.scrollTo(0, document.body.scrollHeight-10000);var lenOfPage=document.body.scrollHeight;return document.body.innerHTML;")
 
     page_soup = BeautifulSoup(innerHTML, "html.parser")
-    mainSection = page_soup.find("section", {"id": "component-topic-us"})
+    mainSection = page_soup.find("div", {"id": "home-page-wrapper"})
     newsColumns = page_soup.find_all(
-        "div", {"class": "component__item-wrapper"})
+        "div", {"class": "home-page-section-stories-wrapper"})
 
     articlesList = []
-    for column in newsColumns[:2]:
-        articles = column.find_all("article")
+    for column in newsColumns:
+        articles = column.find_all("article", {"class": "top-story"})
         for article in articles[:5]:
             try:
                 link = article.find("a")["href"]
-                text = article.find("h4").get_text()
-                text = re.sub("\s\s", "", text)
+                text = article.find("h3", {"class": "headline"}).get_text()
+                text = re.sub("\t", "", text)
                 text = re.sub("\n", "", text)
 
                 article_ = {
-                    "site": "6072168fa55796cad11d99ea",  # Hardcoded ObjectId
+                    "site": "60721d26a55796cad11d99ec",  # Hardcoded ObjectId
                     "headline": text,
                     "article_url": link,
                     "date": today
