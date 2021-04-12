86b9ee1a9facc9efbc2d273c653be61f1adab1bd
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-04-12 10:16:26 -0700

    #11 establish mongodb connection

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,16 @@
 from flask import Flask
+from .dbcontext import connection
+
+# Establish connection
+Context = connection.DbContext
+MongoDbContext = Context().context
+database = MongoDbContext.PyNews
+
+# Collections
+articles = database.articles
+sites = database.sites
+newsdata = database.newsdata
+
 app = Flask(__name__)
 
 
